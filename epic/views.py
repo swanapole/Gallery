07532c9a5ed0b6289cd86ce745d0af68f4a68c0e
swanapole <swanapoleshilla@yahoo.com>
2019-02-25 17:34:30 +0300

    created an convert_dates function to views file for the app

--- epic/views.py
+++ epic/views.py
@@ -3,3 +3,17 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
+
+def convert_dates(dates):
+    '''
+    Function that gets the weekely number for the date.
+    '''
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+    '''
+
+    Returning the actual day of the week
+    '''
+    day = days[day_number]
+    return day
