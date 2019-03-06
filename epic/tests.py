64ad9c8ecfc0a27022425fee7ddca6fbf0772017
swanapole <swanapoleshilla@yahoo.com>
2019-03-06 18:31:53 +0300

    add changes

--- epic/tests.py
+++ epic/tests.py
@@ -5,51 +5,52 @@ from .models import Image,Category,Location
 class CategoryTestCase(TestCase):
 
     def setUp(self):
-        self.food = Category(name = 'delicacies')
+        self.food = Category(name = 'Food')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.delicacies, Category))
+        self.assertTrue(isinstance(self.food, Category))
 
     def tearDown(self):
         Category.objects.all().delete()
 
     def test_save_category(self):
-        self.delicacies.save_category()
+        self.food.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)
 
     def test_delete_category(self):
-        self.delicacies.delete_category('delicacies')
+        self.food.delete_category('Food')
         categories = Category.objects.all()
         self.assertTrue(len(categories) == 0)
+
 class LocationTestCase(TestCase):
     def setUp(self):
         self.nyali = Location(name = 'Nyali')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.sychelles, Location))
+        self.assertTrue(isinstance(self.nyali, Location))
 
     def tearDown(self):
         Location.objects.all().delete()
 
     def test_save_location(self):
-        self.sychelles.save_location()
+        self.nyali.save_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations) > 0)
 
     def test_delete_location(self):
-        self.nyali.delete_location('sychelles')
+        self.nyali.delete_location('Nyali')
         locations = Location.objects.all()
         self.assertTrue(len(locations) == 0)
 
 class ImageTestCase(TestCase):
     def setUp(self):
         # Creating a new category and saving it
-        self.new_category = Category(name = 'delicacies')
+        self.new_category = Category(name = 'Food')
         self.new_category.save_category()
 
         # Creat a new location and saving it
-        self.new_location = Location(name = 'sychelles')
+        self.new_location = Location(name = 'Nyali')
         self.new_location.save_location()
 
         self.new_image = Image(image_name = 'Test Image',image_description = 'Test Description',image_category = self.new_category,image_location = self.new_location)
